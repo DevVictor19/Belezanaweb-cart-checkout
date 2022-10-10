5920f2eecb5e84c37f6a3fc0d13aa5b531b27cf6
Antonio Victor <Antoniovictor12@live.com>
2022-10-10 20:26:45 -0300

    Criando primeiras rotas

---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Checkout } from "./pages/Checkout";
+import { Products } from "./components/products";
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="checkout" element={<Checkout />}>
+          <Route index element={<Products />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
