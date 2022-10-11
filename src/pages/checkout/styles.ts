fc3f36efcdf860a8414049abfa375dce65221955
Antonio Victor <Antoniovictor12@live.com>
2022-10-11 14:12:43 -0300

    colocando padding na div certa

--- src/pages/checkout/styles.ts
+++ src/pages/checkout/styles.ts
@@ -6,12 +6,12 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: ${MAIN_THEME.colors.gray_very_light};
-  padding: 10px;
 `;
 
 export const Content = styled.section`
   margin-top: 40px;
   width: 100%;
+  padding: 10px;
 
   display: flex;
   flex-direction: column;
