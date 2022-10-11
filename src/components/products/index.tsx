829d00d012d8e85a8289a5ecc6552503ea6b1b6d
Antonio Victor <Antoniovictor12@live.com>
2022-10-11 09:42:26 -0300

    renderizando compoenentes de products

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
+import { LabelCard } from "../LabelCard";
+
 export function Products() {
-  return <h1>Meus produtos</h1>;
+  return (
+    <>
+      <LabelCard label="produtos">
+        <div>Produto 1</div>
+        <div>Produto 2</div>
+        <div>Produto 3</div>
+        <div>Produto 4</div>
+      </LabelCard>
+    </>
+  );
 }
