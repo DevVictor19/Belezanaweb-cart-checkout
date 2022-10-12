dfbd56c36886a7bfcb601ade54471e0ea92641a1
Antonio Victor <Antoniovictor12@live.com>
2022-10-12 13:40:23 -0300

    colocando prop type no Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,9 +4,14 @@ import * as S from "./styles";
 
 interface Props {
   label: string;
+  type?: "button" | "submit" | "reset";
   handleClick?: (e: React.MouseEvent) => void;
 }
 
 export function Button(props: Props) {
-  return <S.Button onClick={props.handleClick}>{props.label}</S.Button>;
+  return (
+    <S.Button type={props.type} onClick={props.handleClick}>
+      {props.label}
+    </S.Button>
+  );
 }
