acb269199ce7f6dc8869f75a948e8ba7987e3c9d
Antonio Victor <Antoniovictor12@live.com>
2022-10-12 11:44:11 -0300

    implementando lazyload

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,9 +3,29 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/home";
 import { Checkout } from "./pages/checkout";
-import { CheckoutResume } from "./components/checkout-resume";
-import { CheckoutPayment } from "./components/checkout-payment";
-import { CheckoutConfirmation } from "./components/checkout-confirmation";
+import { Spinner } from "./components/spinner";
+
+const CheckoutResume = React.lazy(() =>
+  import("./components/checkout-resume").then((module) => ({
+    default: module.CheckoutResume,
+  }))
+);
+
+const CheckoutPayment = React.lazy(() =>
+  import("./components/checkout-payment").then((module) => ({
+    default: module.CheckoutPayment,
+  }))
+);
+
+const CheckoutConfirmation = React.lazy(() =>
+  import("./components/checkout-confirmation").then((module) => ({
+    default: module.CheckoutConfirmation,
+  }))
+);
+
+function LazyLoad({ children }: { children: React.ReactNode }) {
+  return <React.Suspense fallback={<Spinner />}>{children}</React.Suspense>;
+}
 
 export function AppRoutes() {
   return (
@@ -13,9 +33,30 @@ export function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="checkout" element={<Checkout />}>
-          <Route index element={<CheckoutResume />} />
-          <Route path="payment" element={<CheckoutPayment />} />
-          <Route path="confirmation" element={<CheckoutConfirmation />} />
+          <Route
+            index
+            element={
+              <LazyLoad>
+                <CheckoutResume />
+              </LazyLoad>
+            }
+          />
+          <Route
+            path="payment"
+            element={
+              <LazyLoad>
+                <CheckoutPayment />
+              </LazyLoad>
+            }
+          />
+          <Route
+            path="confirmation"
+            element={
+              <LazyLoad>
+                <CheckoutConfirmation />
+              </LazyLoad>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
