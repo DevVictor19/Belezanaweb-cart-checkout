392fff2ddc916389ea7c54016f54c545e8f4bc4a
Antonio Victor <Antoniovictor12@live.com>
2022-10-13 16:58:35 -0300

    style: colocar import junto aos outros

--- src/components/checkout-resume/index.tsx
+++ src/components/checkout-resume/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { RootState } from "../../store";
 import { useDispatch, useSelector } from "react-redux";
 
+import { RootState } from "../../store";
 import { IProductsApi } from "../../types/products-api";
 import { ICartProduct } from "../../types/cart-product";
 import { ICartTotalInfo } from "../../types/cart-total-info";
