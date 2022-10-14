7872a67099f160a6c4af6722d7eb86e2501f4c77
Antonio Victor <Antoniovictor12@live.com>
2022-10-14 09:09:34 -0300

    refactor(buttonComp):  add props dinamicas

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,16 +2,10 @@ import React from "react";
 
 import * as S from "./styles";
 
-interface Props {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
-  type?: "button" | "submit" | "reset";
-  handleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export function Button(props: Props) {
-  return (
-    <S.Button type={props.type} onClick={props.handleClick}>
-      {props.label}
-    </S.Button>
-  );
+  return <S.Button {...props}>{props.label}</S.Button>;
 }
